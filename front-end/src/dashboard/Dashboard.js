28b43715af92f7d9d63e6ca22a383b9e823117d6
Bryan Burnett <crankyhippo@gmail.com>
2021-09-09 12:53:51 -0700

    Project Upload

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -1,6 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { listReservations } from "../utils/api";
+import {
+  listReservations,
+  listTables,
+  finishTable,
+  updateStatus,
+} from "../utils/api";
 import ErrorAlert from "../layout/ErrorAlert";
+import { next, previous, today } from "../utils/date-time";
+import { useHistory } from "react-router-dom";
+import ReservationsList from "../reservation/ReservationsList";
+import TablesList from "../tables/TablesList";
 
 /**
  * Defines the dashboard page.
@@ -11,26 +20,108 @@ import ErrorAlert from "../layout/ErrorAlert";
 function Dashboard({ date }) {
   const [reservations, setReservations] = useState([]);
   const [reservationsError, setReservationsError] = useState(null);
+  const [tables, setTables] = useState([]);
+  const history = useHistory();
+  const filterResults = true;
+  const dateOptions = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
 
   useEffect(loadDashboard, [date]);
 
   function loadDashboard() {
     const abortController = new AbortController();
+
     setReservationsError(null);
     listReservations({ date }, abortController.signal)
       .then(setReservations)
       .catch(setReservationsError);
+
+    listTables().then(setTables);
+
     return () => abortController.abort();
   }
 
+  async function finishHandler(table_id) {
+    const abortController = new AbortController();
+    const result = window.confirm(
+      "Is this table ready to seat new guests? This cannot be undone."
+    );
+
+    if (result) {
+      await finishTable(table_id, abortController.signal);
+      loadDashboard();
+    }
+
+    return () => abortController.abort();
+  }
+
+  const cancelHandler = async (event) => {
+    const result = window.confirm(
+      "Do you want to cancel this reservation? This cannot be undone."
+    );
+
+    if (result) {
+      await updateStatus(event.target.value, "cancelled");
+      loadDashboard();
+    }
+  };
+
   return (
     <main>
-      <h1>Dashboard</h1>
-      <div className="d-md-flex mb-3">
-        <h4 className="mb-0">Reservations for date</h4>
-      </div>
       <ErrorAlert error={reservationsError} />
-      {JSON.stringify(reservations)}
+      <div className="group">
+        <div className="item-double">
+          <div className="group">
+            <div className="item-double">
+              <h2>
+                Reservations for{" "}
+                {new Date(date).toLocaleDateString("en-US", dateOptions)}
+              </h2>
+            </div>
+            <div className="item centered">
+              <div className="group-row">
+                <button
+                  className="item yellow"
+                  onClick={() =>
+                    history.push(`/dashboard?date=${previous(date)}`)
+                  }
+                >
+                  Previous
+                </button>
+                <button
+                  className="item yellow"
+                  onClick={() => history.push(`/dashboard?date=${today()}`)}
+                >
+                  Today
+                </button>
+                <button
+                  className="item yellow"
+                  onClick={() => history.push(`/dashboard?date=${next(date)}`)}
+                >
+                  Next
+                </button>
+              </div>
+            </div>
+          </div>
+          <hr></hr>
+          <div id="reservations" className="group-col">
+            <ReservationsList
+              reservations={reservations}
+              filterResults={filterResults}
+              cancelHandler={cancelHandler}
+            />
+          </div>
+        </div>
+        <div id="tables" className="item">
+          <h2>Tables</h2>
+          <hr></hr>
+          <TablesList tables={tables} finishHandler={finishHandler} />
+        </div>
+      </div>
     </main>
   );
 }
